4e80252dca7c725e31ed47320a9e794b3ab2473b
diegoalrv <d.alexis.ramirez@gmail.com>
2024-02-15 00:54:21 -0300

    delete raw Polygon in Project model, added as another model with ForeignKey

--- api/interactive/models/Project.py
+++ api/interactive/models/Project.py
@@ -7,10 +7,9 @@ class Project(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     plates = models.ManyToManyField(Plate, blank=True)
-    area_of_interest = models.ForeignKey(AreaOfInterest, related_name='projects', null=True, blank=True, on_delete=models.SET_NULL)
     discrete_distributions = models.ManyToManyField(DiscreteDistribution, blank=True)
 
-    geometry = models.PolygonField()
+    area_of_interest = models.ForeignKey(AreaOfInterest, related_name='projects', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
