556c43d885564a602aaa021f5f731c0cd79c631b
diegoalrv <d.alexis.ramirez@gmail.com>
2024-02-20 16:58:31 -0300

    fix urls.py

--- api/backend/urls.py
+++ api/backend/urls.py
@@ -1,5 +1,5 @@
 # urls.py
-from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
 router = DefaultRouter()
