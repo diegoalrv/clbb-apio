9e761bc73f3b96418493afadfc97afe0efb1fe15
diegoalrv <d.alexis.ramirez@gmail.com>
2024-02-29 22:55:36 -0300

    added area of interest

---
+++ api/manager/management/commands/load_area_of_interest.py
@@ -0,0 +1,41 @@
+import geopandas as gpd
+from django.contrib.gis.geos import GEOSGeometry
+from backend.models.AreaOfInterest import AreaOfInterest
+from shapely.geometry import Polygon
+
+# Cargar los datos de GeoPandas
+gdf = gpd.read_parquet('/app/assets/area_scope.parquet')
+gdf = gdf.to_crs(4326)
+# Función para convertir Polygon Z a Polygon
+def convert_polygon_z_to_polygon(geometry):
+    if geometry.type == 'Polygon':
+        exterior = Polygon([xy[0:2] for xy in geometry.exterior.coords])
+        interiors = [Polygon([xy[0:2] for xy in interior.coords]) for interior in geometry.interiors]
+        return Polygon(exterior.exterior, [interior.exterior for interior in interiors])
+    else:
+        return geometry  # Retorna la geometría original si no es un Polygon
+
+# Aplicar la función a cada geometría en la columna 'geometry'
+gdf['geometry'] = gdf['geometry'].apply(convert_polygon_z_to_polygon)
+
+# print(gdf)
+# Iterar sobre cada fila y guardar en la base de datos Django
+for index, row in gdf.iterrows():
+    geometry = row.geometry
+
+    # Verificar si la geometría es un polígono con coordenadas (x, y)
+    if isinstance(geometry, Polygon):
+        
+        # print(geometry.wkt)
+        # Convertir la geometría a GEOSGeometry
+        geometry = GEOSGeometry(geometry.wkt)
+        # print(geometry)
+
+        # Crear instancia del modelo LandUses y guardar en la base de datos
+        area_of_interest = AreaOfInterest(
+            name=row['Name'],
+            geo_field=geometry
+        )
+        area_of_interest.save()
+    else:
+        print(f"La geometría en la fila {index} no es un polígono válido con coordenadas (x, y)")
--- api/urban_indicators/views/IndicatorData.py
+++ api/urban_indicators/views/IndicatorData.py
@@ -1,19 +1,36 @@
 import pandas as pd
 import geopandas as gpd
 from django.contrib.gis.geos import GEOSGeometry
+from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from urban_indicators.models.IndicatorData import IndicatorData
 from urban_indicators.serializers.IndicatorData import IndicatorDataSerializer
-from django.db import connection
+import json
+from sqlalchemy import create_engine
+
+def is_geojson(json_data):
+    if isinstance(json_data, dict):
+        if "features" in json_data or "FeatureCollection" in json_data:
+            return True
+    return False
+
+def get_db_url_from_env():
+    import os
+    DB_CONTAINER_NAME = os.getenv('DB_CONTAINER_NAME', None)
+    POSTGRES_USER = os.getenv('POSTGRES_USER', None)
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', None)
+    POSTGRES_DB = os.getenv('POSTGRES_DB', None)
+    DATABASE_URL = os.getenv('DATABASE_URL', None)
+    return f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_CONTAINER_NAME}:5432/{POSTGRES_DB}'
 
 class IndicatorDataViewSet(viewsets.ModelViewSet):
     queryset = IndicatorData.objects.all()
     serializer_class = IndicatorDataSerializer
 
     @action(detail=False, methods=['post'])
-    def create_from_json(self, request):
+    def upload_to_table(self, request):
         indicator_name = request.data.get('indicator_name')
         indicator_hash = request.data.get('indicator_hash')
         table_name = request.data.get('table_name')
@@ -27,30 +44,45 @@ class IndicatorDataViewSet(viewsets.ModelViewSet):
         )
 
         # Procesar y subir los datos JSON a la base de datos
-        process_json_data(indicator_data, json_data)
-
+        self.process_json_data(indicator_data, json_data)
+        indicator_data.save()
         return Response({'message': 'Data saved successfully'})
+    
+    def process_json_data(self, indicator_data, json_data):
+        db_url = get_db_url_from_env()
+        engine = create_engine(db_url)
+        json_data = json.loads(json_data)
+        
+        indicator_name=indicator_data.indicator_name
+        indicator_hash=indicator_data.indicator_hash
+        # table_name = f'{indicator_name}_{indicator_hash}'[:63]
+        table_name = indicator_name[:63]
 
-def is_geojson(json_data):
-    if isinstance(json_data, dict):
-        if "features" in json_data or "FeatureCollection" in json_data:
-            return True
-    return False
+        if is_geojson(json_data):
+            gdf = gpd.GeoDataFrame.from_features(json_data['features'])
+            gdf['geometry'] = gdf['geometry'].apply(lambda x: GEOSGeometry(str(x)))
+            gdf['hash'] = indicator_hash
+            gdf.to_postgis(table_name, engine, if_exists='replace', index=False)
+        else:
+            df = pd.DataFrame(json_data)
+            df['hash'] = indicator_hash
+            df.to_sql(table_name, engine, if_exists='replace', index=False)
+        pass
 
-def process_json_data(indicator_data, json_data):
-    if is_geojson(json_data):
-        # Convertir GeoJSON a GeoDataFrame
-        gdf = gpd.GeoDataFrame.from_features(json_data['features'])
+    @action(detail=False, methods=['get'])
+    def get_table_data(self, request):
+        table_name = request.query_params.get('table_name')
+        indicator_hash = request.query_params.get('indicator_hash')
 
-        # Subir el GeoDataFrame a la base de datos como PostGIS
-        gdf['geometry'] = gdf['geometry'].apply(lambda x: GEOSGeometry(str(x)))
-        gdf.to_postgis(f'indicator__{indicator_data.table_name}', connection, if_exists='append', index=False)
-    else:
-        # Convertir JSON a DataFrame
-        df = pd.DataFrame(json_data)
+        table_data = self.query_table_data(table_name, indicator_hash)
+        return JsonResponse(table_data, safe=False)
 
-        # Subir el DataFrame a la base de datos
-        df.to_sql(f'indicator__{indicator_data.table_name}', connection, if_exists='append', index=False)
-
-    # Se guarda el registro del IndicatorData
-    indicator_data.save()
+    def query_table_data(self, table_name, indicator_hash):
+        db_url = get_db_url_from_env()
+        engine = create_engine(db_url)
+        sql_query = f"""SELECT * FROM "{table_name}" """
+        if indicator_hash:
+            sql_query+=f"""WHERE {table_name}.hash='{indicator_hash}' """
+        df = pd.read_sql_query(sql_query, engine)
+        print(df)
+        return df.to_dict(orient='records')
