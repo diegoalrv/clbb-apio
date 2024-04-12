c0c622274a9ae239b7b8a9eadb1b28e9c2298dc0
diegoalrv <d.alexis.ramirez@gmail.com>
2024-04-12 14:44:58 -0400

    added update table function on indicator data

--- api/urban_indicators/views/IndicatorData.py
+++ api/urban_indicators/views/IndicatorData.py
@@ -8,7 +8,12 @@ from rest_framework.decorators import action
 from urban_indicators.models.IndicatorData import IndicatorData
 from urban_indicators.serializers.IndicatorData import IndicatorDataSerializer
 import json
+from shapely import wkt
+from shapely.geometry import Point
 from sqlalchemy import create_engine
+
+from django.db import connection
+from rest_framework.decorators import action
 
 def is_geojson(json_data):
     if isinstance(json_data, dict):
@@ -31,8 +36,14 @@ class IndicatorDataViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['post'])
     def upload_to_table(self, request):
+        self.post_data_to_table(request)
+        return Response({'message': 'Data saved successfully'})
+    
+    def post_data_to_table(self, request):
         indicator_name = request.data.get('indicator_name')
         indicator_hash = request.data.get('indicator_hash')
+        self.is_geo = request.data.get('is_geo')
+        print(self.is_geo)
         params = request.data.get('params')
         json_data = request.data.get('json_data')
 
@@ -41,13 +52,12 @@ class IndicatorDataViewSet(viewsets.ModelViewSet):
             indicator_name=indicator_name,
             indicator_hash=indicator_hash,
             params=params,
-            is_geo=is_geojson(json_data)
+            is_geo=self.is_geo,
         )
-
         # Procesar y subir los datos (Geo)JSON a la base de datos
         self.process_json_data(indicator_data, json_data)
         indicator_data.save()
-        return Response({'message': 'Data saved successfully'})
+        pass
     
     def process_json_data(self, indicator_data, json_data):
         db_url = get_db_url_from_env()
@@ -57,7 +67,7 @@ class IndicatorDataViewSet(viewsets.ModelViewSet):
         indicator_name=indicator_data.indicator_name
         indicator_hash=indicator_data.indicator_hash
 
-        if is_geojson(json_data):
+        if self.is_geo:
             gdf = gpd.GeoDataFrame.from_features(json_data['features'])
             gdf['hash'] = indicator_hash
             gdf = gdf.set_crs(4326)
@@ -106,10 +116,32 @@ class IndicatorDataViewSet(viewsets.ModelViewSet):
 
     def query_table_data(self):
         self.table_data = None
-        if(self.is_geo):
+        if self.is_geo:
             gdf = gpd.read_postgis(self.sql_query, self.engine, geom_col='geometry')
             self.table_data = json.loads(gdf.to_json())
         else:
             df = pd.read_sql_query(self.sql_query, self.engine)
             self.table_data = df.to_dict(orient='records')
-        pass
+
+    @action(detail=False, methods=['post'])
+    def update_indicator(self, request):
+        indicator_name = request.data.get('indicator_name')
+        indicator_hash = request.data.get('indicator_hash')
+
+        self.delete_indicator(indicator_name, indicator_hash)
+        self.post_data_to_table(request)
+
+        return Response({'message': 'Indicator updated successfully'})
+
+    def delete_indicator(self, indicator_name, indicator_hash):
+        # Eliminar la entrada en IndicatorData
+        indicator_data = IndicatorData.objects.filter(
+            indicator_name=indicator_name,
+            indicator_hash=indicator_hash
+        ).first()
+        if indicator_data:
+            indicator_data.delete()
+
+        # Eliminar la tabla correspondiente
+        with connection.cursor() as cursor:
+            cursor.execute(f"DROP TABLE IF EXISTS {indicator_name}")
