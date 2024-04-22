f1acf575e089bf33ac23ebd529e3613de683e1c4
diegoalrv <d.alexis.ramirez@gmail.com>
2024-04-22 16:56:08 -0400

    update interactive

--- api/interactive/models/Scenario.py
+++ api/interactive/models/Scenario.py
@@ -1,11 +1,14 @@
 from django.contrib.gis.db import models
 from backend.models.LandUse import LandUse
 from backend.models.RoadNetwork import RoadNetwork
+from backend.models.Amenity import Amenity
+from backend.models.GreenArea import GreenArea
 
 class Scenario(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     land_uses = models.ManyToManyField(LandUse, blank=True)
+    
     street_network = models.ForeignKey(RoadNetwork, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
