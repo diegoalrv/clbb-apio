b57a6d937191dbd75777f87c66f3975a2735cb28
diegoalrv <d.alexis.ramirez@gmail.com>
2024-04-23 12:48:05 -0400

    added new fields to scenario (amenities, green areas), change name of geometry in plate

--- api/interactive/models/Scenario.py
+++ api/interactive/models/Scenario.py
@@ -1,11 +1,15 @@
 from django.contrib.gis.db import models
 from backend.models.LandUse import LandUse
 from backend.models.RoadNetwork import RoadNetwork
+from backend.models.Amenity import Amenity
+from backend.models.GreenArea import GreenArea
 
 class Scenario(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     land_uses = models.ManyToManyField(LandUse, blank=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    green_areas = models.ManyToManyField(GreenArea, blank=True)
     street_network = models.ForeignKey(RoadNetwork, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
