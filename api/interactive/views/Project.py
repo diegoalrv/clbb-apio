812565b6dacefc0392f7bf525b908508129df3bc
diegoalrv <d.alexis.ramirez@gmail.com>
2024-04-30 16:15:10 -0400

    added get_data from active plates

--- api/interactive/views/Project.py
+++ api/interactive/views/Project.py
@@ -1,9 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from interactive.models.Project import Project
 from interactive.models.Plate import PlateScenario
 from interactive.serializers.Project import ProjectSerializer
+from interactive.utils.config import data_mapping
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
@@ -20,8 +21,34 @@ class ProjectViewSet(viewsets.ModelViewSet):
         project_status = {}
         
         for plate in plates:
-            # Obtener directamente el Scenario activo, asumiendo que siempre hay uno.
             active_scenario = PlateScenario.objects.get(plate=plate, is_active=True)
             project_status[str(plate.id)] = active_scenario.scenario.id
         
         return Response(project_status)
+
+    @action(detail=True, methods=['get'], url_path='get-data/(?P<data_type>[^/.]+)')
+    def get_data(self, request, pk=None, data_type=None):
+        """
+        Returns data of the specified type from all active scenarios within the plates of the project.
+        """
+        project = self.get_object()
+        active_scenarios = PlateScenario.objects.filter(
+            plate__project=project,
+            is_active=True
+        ).select_related('scenario')
+
+        if data_type in data_mapping:
+            model = data_mapping[data_type]['model']
+            serializer_class = data_mapping[data_type]['serializer']
+            
+            # Collecting all data items from active scenarios
+            data_items = set()
+            for ps in active_scenarios:
+                items = getattr(ps.scenario, data_type).all()
+                for item in items:
+                    data_items.add(item)
+            
+            data = serializer_class(list(data_items), many=True).data
+            return Response(data)
+        else:
+            return Response({"error": "Invalid data type"}, status=status.HTTP_400_BAD_REQUEST)
